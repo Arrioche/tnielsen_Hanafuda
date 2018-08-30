1cea7f324cc3343af399e7932b6cc0c2d6326ea8
Arrioche <kittydurmont@gmail.com>
2018-08-30 09:53:37 -0700

    grsgsdgf

---
+++ CardScreen.java
@@ -0,0 +1,13 @@
+package com.tnielsen9082.hanafuda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CardScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_screen);
+    }
+}
