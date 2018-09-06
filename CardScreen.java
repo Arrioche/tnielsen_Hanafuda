9970f3cb999ec162503d9087c0f908969b7a774a
Arrioche <kittydurmont@gmail.com>
2018-09-06 08:48:31 -0700

    grsgsdgf

--- CardScreen.java
+++ CardScreen.java
@@ -8,6 +8,7 @@ public class CardScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.activity_card_screen);
     }
 }
