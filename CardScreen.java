aaad699c5a69f99f343cf930033591e77675cdb2
Arrioche <kittydurmont@gmail.com>
2018-09-06 10:00:59 -0700

    I have added in all the card images and am now making views to go with them

--- CardScreen.java
+++ CardScreen.java
@@ -1,5 +1,7 @@
 package com.tnielsen9082.hanafuda;
 
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,7 +10,10 @@ public class CardScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         setContentView(R.layout.activity_card_screen);
+        getWindow().getDecorView().setBackgroundColor(Color.parseColor("#228B22"));
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
     }
 }
