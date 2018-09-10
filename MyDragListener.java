6f0db7779fb0da22902dd4a21d86d9de14eee2a2
Arrioche <kittydurmont@gmail.com>
2018-09-10 09:32:49 -0700

    I have the card views, and the skeletons of the drag and drop methods

---
+++ MyDragListener.java
@@ -0,0 +1,71 @@
+package com.tnielsen9082.hanafuda;
+
+import android.content.ClipData;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+//this is the class that the receptacles get
+public final class MyDragListener implements View.OnDragListener {
+    private CharSequence clip;
+    private static final String TAG = "MainActivity";
+    @Override
+    //when a drag is started this activates
+    public boolean onDrag(View v, DragEvent event) {
+        //calls getAction on the drag event
+        //which tells you if it has:
+            /*started
+              entered the box
+              left the box
+              dropped
+              still in the box
+              or force quit
+             */
+        //we don't actually use this though
+        //int action = event.getAction();
+        //a new Java syntax appeared!
+        //it looks for the action like I said
+        //and then performs whichever case matches that action
+        switch (event.getAction()) {
+            case DragEvent.ACTION_DRAG_STARTED:
+                //stop the code
+                break;
+            case DragEvent.ACTION_DRAG_ENTERED:
+                //change the look to show it is droppable in
+                break;
+            case DragEvent.ACTION_DRAG_EXITED:
+                //change the look back
+                break;
+            case DragEvent.ACTION_DROP:
+                // Gets the item containing the dragged data
+                ClipData.Item item = event.getClipData().getItemAt(0);
+                // Gets the text data from the item.
+                clip = item.getText();
+                clip=clip.toString();
+                // Dropped, reassign View to ViewGroup
+                View view = (View) event.getLocalState();
+                //what's a viewGroup?
+                //who knows?
+                //it's a layout thing
+                break;
+            case DragEvent.ACTION_DRAG_ENDED:
+
+            default:
+                //stops if there is no more dragging
+                break;
+        }
+        //this allows you to receive updates on the status of the dragged thing
+        return true;
+    }
+}
+
---
+++ MyTouchListener.java
@@ -0,0 +1,41 @@
+package com.tnielsen9082.hanafuda;
+
+
+import android.content.ClipData;
+
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+
+public final class MyTouchListener implements View.OnTouchListener {
+    //the action that the class performs
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        //makes the class motionEvent perform the getAction function
+        //motionEvents track what is touching the screen
+        //ACTION_DOWN means that someone just started touching it
+        //so this if is checking if the square has been tapped
+        if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+            //the layout that the image being touched is currently in
+            LinearLayout parent = (LinearLayout)view.getParent();
+            //makes a clipData
+            //since we are just using pictures there is no data
+            //but there could be data
+            //this is how you copy/paste things
+            ClipData data;
+            data = ClipData.newPlainText("", view.getContentDescription()+"");
+            //make a custom drag shadow
+            //once I create it
+            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+            view.startDrag(data, shadowBuilder, view, 1);
+            Log.e("TAG",""+data);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
+
